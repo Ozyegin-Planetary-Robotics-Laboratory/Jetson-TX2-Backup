f7f6ed3578ca10b2c9c37b75f79c2c36ba8e7bd0
Tanay Alpkonur <107621923+taannaayyy@users.noreply.github.com>
2024-01-20 18:16:06 +0300

    Update backup_script.sh
    added the feature that compresses home directory to a rar

--- backup_script.sh
+++ backup_script.sh
@@ -1,8 +1,11 @@
 DATE=$(date +\%Y_\%m_\%d_\%H_\%M_\%S)
+
 CHUNK_SIZE=256M
-LOG_PATH="/home/exo/Jetson-TX2-Backup/Backups"
-LOG_FILE="${LOG_PATH}/BACKUP_${DATE}.log"
-mkdir -p ${LOG_PATH}
+
+mkdir -p /home/exo/Backups
+
+# Redirect stdout and stderr to a log file
+LOG_FILE="/home/exo/Backups/BACKUP_${DATE}.log"
 exec > >(tee -a ${LOG_FILE})
 exec 2>&1
 
@@ -13,7 +16,10 @@ log() {
 START_TIME=$(date +%s)
 log "Backup started at: $(date +\%Y-\%m-\%d_\%H:\%M:\%S)"
 
-sudo rclone copy --verbose --drive-chunk-size ${CHUNK_SIZE} /home/exo/ backup:/Jetson-TX2/Backups/EXO/EXO-${DATE}/
+# Compress the home directory using RAR
+rar a -r /home/exo/Backups/home_directory.rar /home/exo/
+
+sudo rclone copy --verbose --drive-chunk-size ${CHUNK_SIZE} /home/exo/Backups/ rover-backup:/Jetson-TX2/Backups/EXO/EXO-${DATE}/
 
 END_TIME=$(date +%s)
 
@@ -31,16 +37,13 @@ if [ $? -eq 0 ]; then
   log "Total space backed up: $BACKED_UP_SPACE"
 
   # Create a summary log file
-  echo "Backup Status: Successful" >> ${LOG_FILE}
-  echo "Start Time: $START_TIME" >> ${LOG_FILE}
-  echo "End Time: $END_TIME" >> ${LOG_FILE}
-  echo "Duration: $(date -u -d @$DURATION +\%H:\%M:\%S)" >> ${LOG_FILE}
-  echo "Total Space Backed Up: $BACKED_UP_SPACE" >> ${LOG_FILE}
+  SUMMARY_LOG="/home/exo/Backups/BACKUP_${DATE}.log"
+  echo "Backup Status: Successful" >> ${SUMMARY_LOG}
+  echo "Start Time: $START_TIME" >> ${SUMMARY_LOG}
+  echo "End Time: $END_TIME" >> ${SUMMARY_LOG}
+  echo "Duration: $(date -u -d @$DURATION +\%H:\%M:\%S)" >> ${SUMMARY_LOG}
+  echo "Total Space Backed Up: $BACKED_UP_SPACE" >> ${SUMMARY_LOG}
 else
   log "Backup failed."
-  echo "Backup Status: Failed" > ${LOG_FILE}
+  echo "Backup Status: Failed" > ${SUMMARY_LOG}
 fi
-
-#copy log file to a drive then delete it. 
-rclone copy ${LOG_FILE} backup:/Jetson-TX2/Backups/EXO/EXO-${DATE}/
-rm ${LOG_FILE}
